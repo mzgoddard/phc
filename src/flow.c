78bf7f1ef8de530d1fc3dcabb5dea43c61173f01
Michael "Z" Goddard <mzgoddard@gmail.com>
2014-05-06 17:17:18 -0400

    Add phflow, initial input handling.

---
+++ src/flow.c
@@ -0,0 +1,27 @@
+#include "ph.h"
+
+void * phFlowCopy(void *_flow, phlist *dst, phlist *src) {
+  // TODO
+  return NULL;
+}
+
+void phFlowCollide(phparticle *a, phparticle *b, phcollision *col) {
+  phflow *flow = a->data;
+  b->acceleration = phAdd(phScale(flow->force, 1 / b->mass), b->acceleration);
+}
+
+phbool phFlowBeforeStep(void *_flow) {
+  phflow *flow = _flow;
+  flow->particle->data = flow;
+  flow->particle->isTrigger = 1;
+  flow->particle->collide = phFlowCollide;
+
+  return 0;
+}
+
+void phFlowUpdate(void *_flow, phworld *world) {}
+
+phconstraintid phflowid = "phflow";
+phconstrainttype *phflowtype = &(phconstrainttype(
+  phflowid, phFlowCopy, phFlowBeforeStep, phFlowUpdate
+));
