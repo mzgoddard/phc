80ad73793104cbdedbf448809e39c50705a04227
Michael "Z" Goddard <mzgoddard@gmail.com>
2014-05-12 23:44:56 -0400

    Add duration bench argument. Stop on SIGINT.

--- example/bench/main.c
+++ example/bench/main.c
@@ -1,4 +1,6 @@
+#include <signal.h>
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 
 #if EMSCRIPTEN
@@ -15,11 +17,21 @@ time_t starttime;
 time_t endtime;
 phbool loop;
 int frames = 0;
+phdouble length = 10.0;
 
 void main_loop();
+void sigint(int);
 
 int main(int argc, char **argv) {
   init();
+
+  if (argc == 2) {
+    if (strncmp(argv[1], "inf\0", 4) == 0) {
+      length = INFINITY;
+    } else {
+      length = atof(argv[1]);
+    }
+  }
 
   // Clock based time is cpu clock. With threads, performance in comparison to
   // cpu clock will always be less than a single main thread that does not need
@@ -33,6 +45,7 @@ int main(int argc, char **argv) {
   #if EMSCRIPTEN
   emscripten_set_main_loop(main_loop, 0, 0);
   #else
+  signal(SIGINT, sigint);
   while (loop) {
     main_loop();
   }
@@ -53,7 +66,7 @@ void main_loop() {
 
   // phdouble t = ((end = clock()) - (phdouble) start) / CLOCKS_PER_SEC;
   phdouble t = difftime(endtime = time(NULL), starttime);
-  if (t >= 10.0) {
+  if (t >= length) {
     loop = 0;
 
     printf("\n");
@@ -66,4 +79,8 @@ void main_loop() {
     exit(0);
     #endif
   }
+}
+
+void sigint(int sig) {
+  length = 0;
 }
